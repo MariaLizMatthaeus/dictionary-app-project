ec541f654ce51d4c20b0e5a25b1fd21aa762140f
MariaLizMatthaeus <94976037+MariaLizMatthaeus@users.noreply.github.com>
2022-06-15 15:45:28 +0200

    inserted dictionary component with search function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo_mm.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button type="button" class="btn btn-primary">
-          Primary
-        </button>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="text-center"> Coded by Maria Matthäus</footer>
     </div>
   );
 }
