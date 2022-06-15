3e478ef181e44eb36759b48e4cadba27b0ec7978
MariaLizMatthaeus <94976037+MariaLizMatthaeus@users.noreply.github.com>
2022-06-15 11:23:53 +0200

    initialized app frame

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo_mm.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
+      </header>
+    </div>
+  );
+}
